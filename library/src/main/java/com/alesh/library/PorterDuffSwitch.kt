105969d6076bef27cdc6df259b570ffacb361983
alesh <d.aleshkov@vironit.co.uk>
2020-05-15 18:55:31 +0300

    added k-doc and specifying comments

--- library/src/main/java/com/alesh/library/PorterDuffSwitch.kt
+++ library/src/main/java/com/alesh/library/PorterDuffSwitch.kt
@@ -52,7 +52,7 @@ class PorterDuffSwitch(context: Context, attrs: AttributeSet) : View(context, at
     private var textPaint = TextPaint(Paint.ANTI_ALIAS_FLAG)
     private var switchPaint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    /* Text measure helpers */
+    /* Text measure helpers (for measure theirs width and height) */
     private lateinit var allTextOpt: TextMeasureHelper
     private lateinit var leftTextOpt: TextMeasureHelper
     private lateinit var rightTextOpt: TextMeasureHelper
@@ -63,9 +63,9 @@ class PorterDuffSwitch(context: Context, attrs: AttributeSet) : View(context, at
     private lateinit var switchRect: Rect
 
     /* Points */
-    private var pointLeft: Float = 0f
-    private var pointRight: Float = 0f
-    private var currentSwitchPosition: Float = 0f
+    private var pointLeft: Float = 0f // left upper corner of switchRect in the left side
+    private var pointRight: Float = 0f // left upper corner of switchRect in the right side
+    private var currentSwitchPosition: Float = 0f // switchRect left upper corner's position
 
     /* Animation completion percentage */
     private var percent = 0f
@@ -175,10 +175,14 @@ class PorterDuffSwitch(context: Context, attrs: AttributeSet) : View(context, at
         canvas?.drawBitmap(rightTextBitmap, pointRight, 0f, textPaint)
     }
 
+    /* Linear interpolation function.
+    Just difference between two point multiplied on time (animation completion percentage in our case)) */
     private fun lerp(startPoint: Float, endPoint: Float, percent: Float): Float {
         return startPoint + (endPoint - startPoint) * percent
     }
 
+    /* Since the lerp functions of moving left and right are different,
+    before each start of the animation we set the lerp function in a separate variable */
     private fun setLerpByCurrentState() {
         currentLerp = if (currentState.isLeft()) {
             { lerp(currentSwitchPosition, pointRight, percent) }
@@ -193,18 +197,43 @@ class PorterDuffSwitch(context: Context, attrs: AttributeSet) : View(context, at
         currentState = currentState.switch()
     }
 
+    /**
+     * @return current state (LEFT or RIGHT) of PorterDuffSwitch
+     */
     fun getState() = currentState
 
+    /**
+     * Register a callback to be invoked when the checked state of this button
+     * changes.
+     *
+     * @param listener the callback to call on checked state change
+     */
     fun setOnStateChangeListener(listener: OnStateChangeListener) {
         onChangeStateListener = listener
     }
 
+    /**
+     * The time interpolator used in calculating the elapsed fraction of this animation. The
+     * interpolator determines whether the animation runs with linear or non-linear motion,
+     * such as acceleration and deceleration. The default value is
+     * {@link android.view.animation.AccelerateDecelerateInterpolator}
+     *
+     * @param value the interpolator to be used by this animation. A value of <code>null</code>
+     * will result in linear interpolation.
+     */
     fun setInterpolator(interpolator: TimeInterpolator) {
         animatorInterpolator = interpolator
         setupAnimation()
     }
 
+    /**
+     * Sets the length of the animation. The default duration is 800 milliseconds.
+     *
+     * @param duration The length of the animation, in milliseconds. This value cannot
+     * be negative.
+     */
     fun setDuration(duration: Long) {
+        if (duration < 0) throw IllegalArgumentException("Animators cannot have negative duration: $duration")
         animatorDuration = duration
         setupAnimation()
     }
